39fc97166948581af558ae7934ada08d6247daec
Smeet1278 <111520170+Smeet1278@users.noreply.github.com>
2022-10-16 17:53:47 +0530

    Add files via upload

---
+++ rankofmatrix (1).cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	int arr[8][8], i, j, n, k, m, r, N, z, R, p, q, v;
+	cin >> n >> m;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	if (arr[0][0] == 0)
+	{
+		for (j = 0; j < m; j++)
+		{
+			int z = -1;
+			for (i = 1; i < n; i++)
+			{
+				if (arr[i][j] != 0)
+				{
+					z = i;
+					break;
+				}
+			}
+			if (z != -1)
+			{
+				for (v = 0; v < m; v++)
+				{
+					int t = arr[0][v];
+					arr[0][v] = arr[i][v];
+					arr[i][v] = t;
+				}
+				break;
+			}
+		}
+	}
+	for (k = 0; k < n - 1; k++)
+	{
+		for (i = k + 1; i < n; i++)
+		{
+			p = arr[k][k];
+			q = arr[i][k];
+			for (j = 0; j < m; j++)
+			{
+				arr[i][j] = p * arr[i][j] - q * arr[k][j];
+			}
+		}
+	}
+	z = 0;
+	for (i = 0; i < n; i++)
+	{
+		int c = 0;
+		for (j = 0; j < m; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				c++;
+			}
+		}
+		if (c == m)
+		{
+			z++;
+		}
+	}
+	R = n - z;
+	cout << "Rank = " << R << endl;
+}
